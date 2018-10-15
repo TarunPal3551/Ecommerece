9d64872aef6344e16d80cee510af4dd79427867f
TarunPal3551 <33154835+TarunPal3551@users.noreply.github.com>
2018-10-15 21:47:39 +0530

    Initial commit

---
+++ app/src/main/java/com/avilaksh/ecomtemplates/product/ItemDetailsActivity.java
@@ -0,0 +1,71 @@
+package com.avilaksh.ecomtemplates.product;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.avilaksh.ecomtemplates.R;
+import com.avilaksh.ecomtemplates.fragments.ImageListFragment;
+import com.avilaksh.ecomtemplates.fragments.ViewPagerActivity;
+import com.avilaksh.ecomtemplates.notification.NotificationCountSetClass;
+import com.avilaksh.ecomtemplates.options.CartListActivity;
+import com.avilaksh.ecomtemplates.startup.MainActivity;
+import com.avilaksh.ecomtemplates.utility.ImageUrlUtils;
+import com.facebook.drawee.view.SimpleDraweeView;
+
+public class ItemDetailsActivity extends AppCompatActivity {
+    int imagePosition;
+    String stringImageUri;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_details);
+        SimpleDraweeView mImageView = (SimpleDraweeView)findViewById(R.id.image1);
+        TextView textViewAddToCart = (TextView)findViewById(R.id.text_action_bottom1);
+        TextView textViewBuyNow = (TextView)findViewById(R.id.text_action_bottom2);
+
+        //Getting image uri from previous screen
+        if (getIntent() != null) {
+            stringImageUri = getIntent().getStringExtra(ImageListFragment.STRING_IMAGE_URI);
+            imagePosition = getIntent().getIntExtra(ImageListFragment.STRING_IMAGE_URI,0);
+        }
+        Uri uri = Uri.parse(stringImageUri);
+        mImageView.setImageURI(uri);
+        mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    Intent intent = new Intent(ItemDetailsActivity.this, ViewPagerActivity.class);
+                    intent.putExtra("position", imagePosition);
+                    startActivity(intent);
+
+            }
+        });
+
+        textViewAddToCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImageUrlUtils imageUrlUtils = new ImageUrlUtils();
+                imageUrlUtils.addCartListImageUri(stringImageUri);
+                Toast.makeText(ItemDetailsActivity.this,"Item added to cart.",Toast.LENGTH_SHORT).show();
+                MainActivity.notificationCountCart++;
+                NotificationCountSetClass.setNotifyCount(MainActivity.notificationCountCart);
+            }
+        });
+
+        textViewBuyNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImageUrlUtils imageUrlUtils = new ImageUrlUtils();
+                imageUrlUtils.addCartListImageUri(stringImageUri);
+                MainActivity.notificationCountCart++;
+                NotificationCountSetClass.setNotifyCount(MainActivity.notificationCountCart);
+                startActivity(new Intent(ItemDetailsActivity.this, CartListActivity.class));
+
+            }
+        });
+
+    }
+}
